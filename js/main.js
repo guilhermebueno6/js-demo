c955c76489ed3d9ae2a778f86ff8d89dbf54eabd
Guilherme <guilhermebueno6@gmail.com>
2020-05-22 16:07:16 -0400

    fix

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@
 
 function loggin(){
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    console.log(w)
+  
     var x = event.clientX
-    console.log(x)
+ 
     var newX = x/16
     var roundedX = Math.min(90,Math.floor(newX))
-    console.log(roundedX);
+ 
     
     // if (roundedX>90){
     //     return
@@ -30,3 +30,30 @@ function toggle() {
 
 var openMenu = document.getElementById('nav-toggler');
 openMenu.addEventListener("click", toggle);
+
+
+function preloader() {
+	if (document.getElementById) {
+        let i = 0;
+        while (i<=90){
+            
+        document.getElementById("preload").style.background = `url(./public/images/MainComp/MainComp_000${i}.png) no-repeat -9999px -9999px`;
+    i=i+1}
+        
+		
+	}
+}
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			if (oldonload) {
+				oldonload();
+			}
+			func();
+		}
+	}
+}
+addLoadEvent(preloader);
